3797a4d7c7c9bf7b4f7dae1b98ec4dab85960779
Prakhar Sharma <prakhar.156clg@gmail.com>
2025-06-16 15:47:57 +0530

    Fix url auth 7

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 exports.auth = (req, res, next) => {
     try {
-        const token = req.body.token || req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
-
+        // const token = req.body.token || req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
+        const token =  req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
         if (!token) {
             return res.status(401).json({ success: false, message: 'Token Missing' });
         }

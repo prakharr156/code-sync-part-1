4bfc1abb600b564e4e88f519d51c9cb8a859c5bd
Prakhar Sharma <prakhar.156clg@gmail.com>
2025-06-17 01:45:28 +0530

    Fix url auth 12

--- backend/Controller/Auth.js
+++ backend/Controller/Auth.js
@@ -56,15 +56,17 @@ exports.login = async (req, res) => {
             const options = {
                 expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
                 httpOnly: true,
+                secure: true,           // required for HTTPS (Render)
+                sameSite: 'None'
             };
 
             return res.cookie("token", token, options).status(200).json({
+                // httpOnly:true,
                 success: true,
                 token,
                 user,
                 message: "User logged in successfully",
-                 secure: true,           // required for HTTPS (Render)
-                sameSite: 'None'
+                 
             });
         } else {
             return res.status(403).json({ success: false, message: "Incorrect password" });

30cf4006778d5cbb32feb761d9de241551f6fd78
Prakhar Sharma <prakhar.156clg@gmail.com>
2025-06-17 01:25:08 +0530

    Fix url auth 11

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { login, signup } = require("../Controller/Auth");
+const { login, signup , logout} = require("../Controller/Auth");
 const { auth } = require("../middleware/auth");
 
 router.post("/login", login);
